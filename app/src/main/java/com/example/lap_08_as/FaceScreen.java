39804224ff47dc9bd549a8e5c1ac1aa1a5cc4685
SoulEzz <vuaharem1.0@gmail.com>
2022-04-24 22:42:39 +0700

    Firebase

---
+++ app/src/main/java/com/example/lap_08_as/FaceScreen.java
@@ -0,0 +1,14 @@
+package com.example.lap_08_as;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FaceScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_screen);
+    }
+}
